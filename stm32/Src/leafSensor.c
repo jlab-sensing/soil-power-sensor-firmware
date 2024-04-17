7f842a6b92ca4fe6cbf047aafc34ce6ad3e44510
gxjacobs <166069663+gxjacobs@users.noreply.github.com>
2024-04-17 10:37:54 -0700

    created blank leafSensor.c for leaf wetness sensor

---
+++ stm32/Src/leafSensor.c
@@ -0,0 +1,14 @@
+/**
+  ******************************************************************************
+  * @file    leafSensor.c
+  * @author  Caden Grace Jacobs
+  * @brief   Soil Power Sensor ADS12 library
+  *          This file provides a function to read from the onboard ADC (ADS1219)
+  *          to detect the reading from the phytos 31
+  * @date    4/17/2024
+  *
+  ******************************************************************************
+  **/
+
+/* Includes ------------------------------------------------------------------*/
+#include "ads.h"
